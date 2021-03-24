83e4adbd59cccec836015fe8d7f332efedb6246e
Daniel Haugen <me@dnhaugen.com>
2021-03-24 16:48:23 -0700

    fixed linter and syntax

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 'use strict';
 
 // Product Construction function
-function Product (name, imgSrc) {
+function Product(name, imgSrc) {
   this.name = name;
   this.url = imgSrc;
   this.timesShown = 0;
   this.timesClicked = 0;
   Product.all.push(this);
-};
-
-
+}
 
 //  Global Variables
 let lImg = null;
@@ -18,9 +16,8 @@ let rImg = null;
 const roundsToPlay = 5; // DANGER DANGER DANGER
 let totalClicks = 0;
 Product.all = [];
-
-
-
+let butt = null;
+const resultsSection = document.getElementById('resultList');
 
 //shuffle an array randomly and Create Element functions
 function shuffle(array) {
@@ -30,19 +27,17 @@ function shuffle(array) {
   }
 }
 
-function createEl(tag, parent, id, text,) {
+function createEl(tag, parent, id, text) {
   const el = document.createElement(tag);
   parent.appendChild(el);
-  if(id !== undefined) {
+  if (id !== undefined) {
     el.id = id;
   }
   if (text !== undefined) {
     el.textContent = text;
-  } 
+  }
   return el;
 }
-
-
 
 // DOM Elements to update
 const prodCtr = document.getElementById('product-ctr');
@@ -55,36 +50,48 @@ const leftHeadingEl = document.getElementById('lHeading');
 const middleHeadingEl = document.getElementById('mHeading');
 const rightHeadingEl = document.getElementById('rHeading');
 
-
-
 // Pick Products function
 function pickProducts() {
-
- const prevLeft = lImg;
- const prevMiddle = mImg;
- const prevRight = rImg;
+  const prevLeft = lImg;
+  const prevMiddle = mImg;
+  const prevRight = rImg;
 
   shuffle(Product.all);
 
- for(let product of Product.all) {
-    if(product !== prevLeft && product !== prevMiddle && product !== prevRight) {
+  for (let product of Product.all) {
+    if (
+      product !== prevLeft &&
+      product !== prevMiddle &&
+      product !== prevRight
+    ) {
       lImg = product;
       break;
     }
   }
 
   for (let product of Product.all) {
-    if (product !== prevLeft && product !== prevMiddle && product !== prevRight && product !== lImg) {
+    if (
+      product !== prevLeft &&
+      product !== prevMiddle &&
+      product !== prevRight &&
+      product !== lImg
+    ) {
       mImg = product;
-      break
+      break;
     }
   }
 
   for (let product of Product.all) {
-    if (product !== prevLeft && product !== prevMiddle && product !== prevRight && product !== lImg && product !== mImg) {
+    if (
+      product !== prevLeft &&
+      product !== prevMiddle &&
+      product !== prevRight &&
+      product !== lImg &&
+      product !== mImg
+    ) {
       rImg = product;
       break;
-    } 
+    }
   }
 
   for (let product of Product.all) {
@@ -103,25 +110,11 @@ Product.prototype.render = function () {
 
   rightImageEl.src = rImg.url;
   rightHeadingEl.textContent = rImg.name;
-}
-
-
-// const renderNewProducts = function () {
-//   leftImageEl.src = lImg.url;
-//   leftHeadingEl.textContent = lImg.name;
-
-//   middleImageEl.src = mImg.url;
-//   middleHeadingEl.textContent = mImg.name;
-
-//   rightImageEl.src = rImg.url;
-//   rightHeadingEl.textContent = rImg.name;
-
-// }
-
+};
 
 // Create Products
 new Product('Bag', 'img/bag.jpg');
-let banana = new Product('Banana', 'img/banana.jpg');
+new Product('Banana', 'img/banana.jpg');
 new Product('Bathroom', 'img/bathroom.jpg');
 new Product('Boots', 'img/boots.jpg');
 new Product('Breakfast', 'img/breakfast.jpg');
@@ -143,66 +136,86 @@ new Product('Wine Glass', 'img/wine-glass.jpg');
 
 // First function call
 pickProducts();
+renderChart();
 
 //add event listener
 prodCtr.addEventListener('click', handleClickOnProduct);
 
-
 //event handler
 function handleClickOnProduct(event) {
-  if(totalClicks < roundsToPlay) {
+  if (totalClicks < roundsToPlay) {
     const productSelected = event.target;
     const id = productSelected.id;
     lImg.timesShown++;
     mImg.timesShown++;
     rImg.timesShown++;
 
-    if(id === 'lImage' || id === 'mImage' || id === 'rImage'  ) {
-      if(id === 'lImage') {
+    if (id === 'lImage' || id === 'mImage' || id === 'rImage') {
+      if (id === 'lImage') {
         totalClicks++;
         lImg.timesClicked++;
-       
       } else if (id === 'mImage') {
         totalClicks++;
         mImg.timesClicked++;
-        
       } else {
         totalClicks++;
         rImg.timesClicked++;
       }
     }
 
-    if(totalClicks < roundsToPlay) {
+    if (totalClicks < roundsToPlay) {
       pickProducts();
-  } else {
-    prodCtr.removeEventListener('click', handleClickOnProduct);
-    alert('yo you\'re done');
-   
-    renderButton();
-    // butt.removeEventListener('click', renderResults);
-    const butt = document.getElementById('viewResults');
-    console.log(butt);
-    butt.addEventListener('click', renderResults);
-  }
-} 
-}
+    } else {
+      prodCtr.removeEventListener('click', handleClickOnProduct);
+      alert('yo you\'re done');
 
-const resultsSection = document.getElementById('resultList');
+      renderButton();
+
+      butt = document.getElementById('viewResults');
+      butt.addEventListener('click', renderResults);
+    }
+  }
+}
 
 function renderResults() {
   for (let product of Product.all) {
-    createEl('li', resultsSection, undefined, `${product.name} was shown ${product.timesShown} times and selected ${product.timesClicked}.`)
+    createEl(
+      'li',
+      resultsSection,
+      undefined,
+      `${product.name} was shown ${product.timesShown} times and selected ${product.timesClicked}.`
+    );
   }
-  const butt = document.getElementById('viewResults');
   butt.removeEventListener('click', renderResults);
 }
+
+
 
 function renderButton() {
   createEl('button', resultsSection, 'viewResults', 'View Results');
 }
 
+function renderChart() {
+  var ctx = document.getElementById('chart').getContext('2d');
+  new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
 
+    // The data for our dataset
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    },
 
-
-
+    // Configuration options go here
+    options: {},
+  });
+}
 
